234a1dd53f0ebc6385243630bccb1030d590702e
jiwon026 <151740502+jiwon026@users.noreply.github.com>
2025-11-29 17:52:31 +0900

    Update veiw.py

--- veiw.py
+++ veiw.py
@@ -5,6 +5,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import matplotlib.pyplot as plt 
+import seaborn as sns
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from sklearn.model_selection import train_test_split
